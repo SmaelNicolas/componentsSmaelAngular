1b1ce41680264013ccec4e68574a40cae8f22ba4
Smael Nicolas <nico.smael@gmail.com>
2022-03-17 13:10:53 -0300

    app component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,21 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { LeftMenuComponent } from './components/left-menu/left-menu.component';
+import { ContentAreaComponent } from './components/content-area/content-area.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    LeftMenuComponent,
+    ContentAreaComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
